5d22059cbf3ca00bc79d0ebf288e6264b3f66164
prasanth <kvsprasanth007@gmail.com>
2025-04-05 00:23:02 -0400

    Remove unnecessary binary files and IDE configuration

--- binarySe.java
+++ binarySe.java
@@ -1,26 +1,35 @@
-public class binarySe {
-    public int binarySearch(int[] nums, int target){
+public  class binarySe {
+    public int lastOccurrence(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
+        int result = -1;
 
-        while(left <= right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
 
-
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
+            if (nums[mid] == target) {
+                result = mid; // store answer
+                left = mid + 1; // go right to find later target
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        return -1;
+
+        return result;
     }
+
+
+
     public static void main(String[] args) {
         binarySe bs = new binarySe();
         int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         int target = 5;
-        int result = bs.binarySearch(nums, target);
+        int result = bs.lastOccurrence(nums, target);
         if (result != -1) {
-            System.out.println("Element " + target + " found at index: " + result);
+            System.out.println("The last occurrence of " + target + " is at index: " + result);
         } else {
-            System.out.println("Element " + target + " not found in the array.");
+            System.out.println("The target " + target + " is not found in the array.");
         }
     }
 }
